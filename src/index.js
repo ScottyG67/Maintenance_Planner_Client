042431cee8b4e88d74700673de2937b562c0fb59
github username <scott.gloyna01@gmail.com>
2021-06-05 15:52:08 -0500

    tested api fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+const BASE_URL = "http://localhost:3000/"
 
 document.addEventListener("DOMContentLoaded",()=>{
+
   console.log("dom loaded")
   insertWelcome()
+  addTestButn()
 })
 
 function insertWelcome() {
@@ -9,4 +12,27 @@ function insertWelcome() {
   let welcome = document.createElement('h2')
   welcome.innerText="This was added in the Index.js file"
   root.appendChild(welcome)
+}
+
+function addTestButn() {
+  const root = document.getElementById("root")
+  let button = document.createElement('button')
+  button.innerText= "click to show list of plants"
+  button.addEventListener('click',loadPlants)
+  root.appendChild(button)
+}
+
+function loadPlants() {
+  fetch('http://localhost:3000/plants').then(res=>res.json()).then(plantsList => renderPlantsList(plantsList))
+}
+
+function renderPlantsList(plantsList){
+  const root = document.getElementById("root")
+  const ul = document.createElement('ul')
+  root.appendChild(ul)
+  plantsList.forEach(plant => {
+    let li = document.createElement('li')
+    li.innerText=plant.name
+    ul.appendChild(li)
+  });
 }
